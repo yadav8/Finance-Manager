e0864ae7a2cee476a54566590687f0c37c197b87
yadav8 <sumitpyadav@gmail.com>
2020-02-06 00:42:06 -0500

    login working with databse. Registration next

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # Creating a flask instance - app
 app = Flask(__name__) 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 # The 'app' folder
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,18 +1,39 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+# Creating a user_loader function for the flask_login extension
+# to be able to load a user from our DB
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+	# Users DB fields
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
+
+	# User DB relationships
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 
 	def __repr__(self):
-		return '<User {}>'.format(self.username)   
+		return '<User {}>'.format(self.username)
+
+	# Converts plaintext password to hash
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	# Checks input plaintext password against hashed password
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)   
 
 
 class Post(db.Model):
+	# Posts DB fields
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.String(140))
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
